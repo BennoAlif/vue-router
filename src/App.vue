835a9ca358026d30bf077c186347b36b1f698ffe
Codelabs Division <codelabs@Apples-MacBook-Pro.local>
2019-08-01 11:42:04 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <ul>
+        <li v-for="(id, index) in userIds" :key=(index)>
+          <router-link :to="{name : 'users', params:{user_id : id}}">
+            <span>User {{id}}</span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      userIds : ['1', '2', '3', '4']
+    }
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav ul{
+  list-style-type: none;
+  padding: 0;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Users.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="users">
+        <h1>User {{ userId }}</h1>
+        <button @click="goHome()">Go Home</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'users',
+    data(){
+        return{
+            userId : this.$route.params.user_id
+        }
+    },
+    methods : {
+        updateId(){
+            this.userId = this.$route.params.user_id
+        },
+        goHome(){
+            this.$router.push({name: 'home'})
+            // this.$router.go(-1)
+        }
+    },
+    watch : {
+        $route : 'updateId'
+    }
+}
+</script>
